47474a1ee0297b9e3e8582fb720c3120679a5923
cris-dangithub <cristiandaniel8080@gmail.com>
2023-01-28 13:19:12 -0500

    changing files

--- 02-Installation/src/utils/ThreeJS/creatingScene.js
+++
@@ -1,8 +0,0 @@
-import { BoxGeometry, Mesh, MeshBasicMaterial } from "three";
-
-export const createCube = () => {
-  const geometry = new BoxGeometry(1, 1, 1);
-  const material = new MeshBasicMaterial({ color: "blue" });
-  const cube = new Mesh(geometry, material);
-  return cube;
-};
